4463be2ab8d96e1d992184cce7d7827e3b59384d
jh <jh@2mbgame.com>
2016-11-29 08:32:32 +0900

    - editor tools - Getting names of AssetBundles - Getting told when an asset changes AssetBundle

---
+++ Assets/Editor/MyPostprocessor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MyPostprocessor : AssetPostprocessor
+{
+
+    void OnPostprocessAssetbundleNameChanged(string path,
+                                             string previous, 
+                                             string next)
+    {
+        Debug.Log("AB: " + path + " old: " + previous + " new: " + next);
+    }
+}
